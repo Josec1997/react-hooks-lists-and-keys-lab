be0d4660213f32136c59b5bec4f29e05eadbd9c9
Jose Campos <Jose_cmps14@yahoo.com>
2023-02-10 21:16:31 -0600

    commit tree 6c462ac3409d15cf0153a9090129426a0eece43f

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,10 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
+  const linkTag= links.map((link) => {
+    return <a key={link} href={link.id}>{link}</a>
+  })
+
   return <nav>{/* display an <a> tag for each link here */}</nav>;
 }
 
